82d6e2318a367bd935d361fc387d860cafc5495e
yaya-cisse <yaya-cisse.abdelhafiz@mobile-devices.fr>
2016-05-18 17:28:33 +0200

    ajout des creates et des edits

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,15 +12,17 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  # POST /users
+  # POST /users.json
   def create
     @user = User.new(name: params[:name], status: params[:status])
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'Post was successfully created.' }
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render json: @user, status: :created, location: @user }
       else
-        format.html { render action: "new" }
+        format.html { render action: "index" }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
@@ -31,6 +33,22 @@ class UsersController < ApplicationController
     respond_to do |format|
       format.html #index.html.erb
       format.json {render :json => @users}
+    end
+  end
+
+  # PUT /users/1
+  # PUT /users/1.json
+  def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(name: params[:name], status: params[:status])
+        format.html { redirect_to @user, notice: 'Post was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "show" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
   end
 
